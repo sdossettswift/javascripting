1ce464bf548b57470fde77a060264f9bba2f97ea
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-15 15:27:45 -0500

    working through nodeschool's javascripting

---
+++ for-loop.js
@@ -0,0 +1,6 @@
+var total = 0
+var limit = 10
+
+for (var total; total < limit; total += 1) {
+  console.log(total); 
+}
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+var pizza = "pizza is alright";
+example = pizza.replace("alright", "wonderful"); 
+console.log(example); 
