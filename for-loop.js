7695cf7f1af50730b495c6d3b5283b1f31cc4681
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-15 17:47:30 -0500

    for loop

--- for-loop.js
+++ for-loop.js
@@ -1,6 +1,9 @@
 var total = 0
 var limit = 10
+var i = 0
 
-for (var total; total < limit; total += 1) {
-  console.log(total); 
+for (var i = 0; i< limit; i+=1 ) {
+  total += i; 
 }
+
+console.log(total) 
